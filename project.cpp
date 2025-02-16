ab6ee55f192090de96fe0b502a86a93f79c6a822
0xchenxuezhen <0xchenxuezhen@gmail.com>
2025-02-16 13:10:50 +0800

    Modify project.cpp

--- project.cpp
+++ project.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Every challenge way.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
