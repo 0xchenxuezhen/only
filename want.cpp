6d23292c700810f44e52283c3800263d1e6d357e
0xchenxuezhen <0xchenxuezhen@gmail.com>
2025-02-23 15:27:11 +0800

    Modify want.cpp

--- want.cpp
+++ want.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Range animal fine respond product.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
